f2bb5a5e2b3863012e8a02e7a2ad145f95266add
oguiza <oguiza@gmail.com>
2020-04-10 11:13:31 +0200

    🔥 fastai v2

--- tsai/_nbdev.py
+++ tsai/_nbdev.py
@@ -84,8 +84,8 @@ modules = ["utils.py",
            "models/ResNet.py",
            "models/InceptionTime.py"]
 
-doc_url = "https://timeseriesai.github.io/timeseriesAI//"
+doc_url = "https://oguiza.github.io/"
 
-git_url = "https://github.com/timeseriesAI/timeseriesAI/tree/master/"
+git_url = "https://github.com/oguiza/tsai/tree/master/"
 
 def custom_doc_links(name): return None
