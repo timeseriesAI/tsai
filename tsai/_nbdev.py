03ea8fcf4e29db499cf28b5a0729a0b3737ebe3a
“oguiza” <oguiza@gmail.com>
2020-11-06 15:18:26 +0100

    old models cleanup

--- tsai/_nbdev.py
+++ tsai/_nbdev.py
@@ -439,7 +439,7 @@ modules = ["utils.py",
            "models/Hybrid.py",
            "trading/utils.py"]
 
-doc_url = "https://timeseriesai.github.io//timeseriesAI/"
+doc_url = "https://timeseriesai.github.io//tsai/"
 
 git_url = "https://github.com/timeseriesAI/timeseriesAI/tree/master/"
 
