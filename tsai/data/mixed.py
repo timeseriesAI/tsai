db61953eef22c3c20de50fa2d6d21445e3720e5a
“oguiza” <oguiza@gmail.com>
2021-12-04 19:45:03 +0100

    fixes #229

--- tsai/data/mixed.py
+++ tsai/data/mixed.py
@@ -96,7 +96,9 @@ class MixedDataLoader():
                 outs += batch[dl.n_inp:]
             inps = tuple([tuple(L(inps)[idx]) if isinstance(idx, list) else inps[idx]
                           for idx in self.x_idxs]) if len(self.x_idxs) > 1 else tuple(L(outs)[self.x_idxs][0])
-            outs = tuple(L(outs)[self.y_idxs]) if len(self.y_idxs) > 1 else L(outs)[self.y_idxs][0]
+            if len(self.y_idxs) == 0: # based on issue identified by @Wabinab https://github.com/timeseriesAI/tsai/pull/229
+                yield tuple((inps,))
+            outs =  tuple(L(outs)[self.y_idxs]) if len(self.y_idxs) > 1 else L(outs)[self.y_idxs][0]
             yield inps, outs
 
     def one_batch(self):
