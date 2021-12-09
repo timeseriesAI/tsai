d4262e28511b069be8e46a594ae0b24503db7084
“oguiza” <oguiza@gmail.com>
2021-12-09 19:36:35 +0100

    fixes #307

--- tsai/data/preparation.py
+++ tsai/data/preparation.py
@@ -317,28 +317,33 @@ def add_delta_timestamp_cols(df, cols=None, groupby=None, forward=True, backward
 # # SlidingWindow vectorization is based on "Fast and Robust Sliding Window Vectorization with NumPy" by Syafiq Kamarul Azman
 # # https://towardsdatascience.com/fast-and-robust-sliding-window-vectorization-with-numpy-3ad950ed62f5
 
-def SlidingWindow(window_len:int, stride:Union[None, int]=1, start:int=0, pad_remainder:bool=False, padding_value:float=np.nan, add_padding_feature:bool=True,
-                  get_x:Union[None, int, list]=None, get_y:Union[None, int, list]=None, y_func:Optional[callable]=None, copy:bool=False,
-                  horizon:Union[int, list]=1, seq_first:bool=True, sort_by:Optional[list]=None, ascending:bool=True, check_leakage:bool=True):
+def SlidingWindow(window_len:int, stride:Union[None, int]=1, start:int=0, pad_remainder:bool=False, padding:str="post", padding_value:float=np.nan,
+                  add_padding_feature:bool=True, get_x:Union[None, int, list]=None, get_y:Union[None, int, list]=None, y_func:Optional[callable]=None,
+                  output_filter:Optional[callable]=None, copy:bool=False, horizon:Union[int, list]=1, seq_first:bool=True, sort_by:Optional[list]=None,
+                  ascending:bool=True, check_leakage:bool=True):
 
     """
     Applies a sliding window to a 1d or 2d input (np.ndarray, torch.Tensor or pd.DataFrame)
     Args:
         window_len          = length of lookback window
         stride              = n datapoints the window is moved ahead along the sequence. Default: 1. If None, stride=window_len (no overlap)
-        start               = determines the step where the first window is applied: 0 (default), a given step (int), or random within the 1st stride (None).
+        start               = determines the step where the first window is applied: 0 (default) or a given step (int). Previous steps will be discarded.
         pad_remainder       = allows to pad remainder subsequences when the sliding window is applied and get_y == [] (unlabeled data).
+        padding             = 'pre' or 'post' (optional, defaults to 'pre'): pad either before or after each sequence. If pad_remainder == False, it indicates
+                              the starting point to create the sequence ('pre' from the end, and 'post' from the beginning)
         padding_value       = value (float) that will be used for padding. Default: np.nan
         add_padding_feature = add an additional feature indicating whether each timestep is padded (1) or not (0).
-        horizon             = number of future datapoints to predict:
+        horizon             = number of future datapoints to predict (y). If get_y is [] horizon will be set to 0.
                             * 0 for last step in each sub-window.
                             * n > 0 for a range of n future steps (1 to n).
                             * n < 0 for a range of n past steps (-n + 1 to 0).
                             * list : for those exact timesteps.
         get_x               = indices of columns that contain the independent variable (xs). If None, all data will be used as x.
         get_y               = indices of columns that contain the target (ys). If None, all data will be used as y.
-                            [] means no y data is created (unlabeled data).
-        y_func              = function to calculate the ys based on the get_y col/s and each y sub-window. y_func must be a function applied to axis=1!
+                              [] means no y data is created (unlabeled data).
+        y_func              = optional function to calculate the ys based on the get_y col/s and each y sub-window. y_func must be a function applied to axis=1!
+        output_filter       = optional function to filter output (X (and y if available)). This is useful when some values need to be removed. The function
+                              should take X and y (even if it's None) as arguments.
         copy                = copy the original object to avoid changes in it.
         seq_first           = True if input shape (seq_len, n_vars), False if input shape (n_vars, seq_len)
         sort_by             = column/s used for sorting the array in ascending order
@@ -349,6 +354,7 @@ def SlidingWindow(window_len:int, stride:Union[None, int]=1, start:int=0, pad_re
         shape: (seq_len, ) or (seq_len, n_vars) if seq_first=True else (n_vars, seq_len)
     """
 
+    if get_y == []: horizon = 0
     if horizon == 0: horizon_rng = np.array([0])
     elif is_listy(horizon): horizon_rng = np.array(horizon)
     elif isinstance(horizon, Integral): horizon_rng = np.arange(1, horizon + 1) if horizon > 0 else np.arange(horizon + 1, 1)
@@ -361,6 +367,7 @@ def SlidingWindow(window_len:int, stride:Union[None, int]=1, start:int=0, pad_re
         'you need to change either horizon, get_x, get_y or use a y_func to avoid leakage'
     if stride == 0 or stride is None:
         stride = window_len
+    if pad_remainder: assert padding in ["pre", "post"]
 
     def _inner(o):
         if copy:
@@ -384,43 +391,63 @@ def SlidingWindow(window_len:int, stride:Union[None, int]=1, start:int=0, pad_re
             if get_y == []: y = None
             elif get_y is None: y = o
             else: y = o[:, _get_y]
-        seq_len = len(X)
-        if get_y != []:
-            X_max_time = seq_len - start - window_len - max_horizon - pad_remainder
+
+        # X
+        if start != 0:
+            X = X[start:]
+        X_len = len(X)
+        if not pad_remainder:
+            if X_len < window_len + max_horizon:
+                return None, None
+            else:
+                n_windows = 1 + (X_len - max_horizon - window_len) // stride
         else:
-            X_max_time = seq_len - start - window_len - pad_remainder
-        if pad_remainder:
+            n_windows = 1 + max(0, np.ceil((X_len - max_horizon - window_len) / stride).astype(int))
+        X_max_len = window_len + max_horizon + (n_windows - 1) * stride # total length required (including y)
+        X_seq_len = X_max_len - max_horizon
+
+        if pad_remainder and X_len < X_max_len:
             if add_padding_feature:
                 X = np.concatenate([X, np.zeros((X.shape[0], 1))], axis=1)
-            X_max_time = X_max_time - X_max_time % stride + stride
-            if window_len + start + X_max_time - len(X) >= 0:
-                _X = np.empty((window_len + start + X_max_time - len(X), *X.shape[1:]))
-                _X[:] = padding_value
-                if add_padding_feature:
-                    _X[:, -1] = 1
+            _X = np.empty((X_max_len - X_len, *X.shape[1:]))
+            _X[:] = padding_value
+            if add_padding_feature:
+                _X[:, -1] = 1
+            if padding == "pre":
+                X = np.concatenate((_X, X))
+            elif padding == "post":
                 X = np.concatenate((X, _X))
-        elif X_max_time < 0: return None, None
-        X_sub_windows = (start +
-                         np.expand_dims(np.arange(window_len), 0) + # window len
-                         np.expand_dims(np.arange(X_max_time + 1, step=stride), 0).T
-                        ) # # subwindows
+        if padding == "pre":
+            X_start = X_len - X_max_len
+            X = X[-X_max_len:-X_max_len + X_seq_len]
+        elif padding == "post":
+            X_start = 0
+            X = X[:X_seq_len]
+
+        X_sub_windows = (np.expand_dims(np.arange(window_len), 0) +
+                         np.expand_dims(np.arange(n_windows * stride, step=stride), 0).T)
         X = np.transpose(X[X_sub_windows], (0, 2, 1))
+
+        # y
         if get_y != [] and y is not None:
-            y_start = start + window_len - 1
-            y_max_time = seq_len - y_start - max_horizon - pad_remainder
-            div = 0
-            if pad_remainder:
-                div = y_max_time % stride
-                y_max_time = y_max_time - y_max_time % stride + stride
-                if window_len + start + y_max_time - len(y) >= 0:
-                    _y = np.empty((window_len + start + y_max_time - len(y), *y.shape[1:]))
-                    _y[:] = padding_value
+            y_start = start + X_start + window_len + min_horizon - 1
+            y_max_len = max_horizon - min_horizon + 1 + (n_windows - 1) * stride
+            y = y[y_start:y_start + y_max_len]
+            y_len = len(y)
+            y_seq_len = y_max_len
+
+            if pad_remainder and y_len < y_max_len:
+                _y = np.empty((y_max_len - y_len, *y.shape[1:]))
+                _y[:] = padding_value
+                if padding == "pre":
+                    y = np.concatenate((_y, y))
+                elif padding == "post":
                     y = np.concatenate((y, _y))
-            y_sub_windows = (y_start +
-                             np.expand_dims(horizon_rng, 0) + # horizon_rng
-                             np.expand_dims(np.arange(y_max_time + div, step=stride), 0).T
-                            ) # # subwindows
+
+            y_sub_windows = (np.expand_dims(horizon_rng - min_horizon, 0)+
+                             np.expand_dims(np.arange(n_windows * stride, step=stride), 0).T)
             y = y[y_sub_windows]
+
             if y_func is not None and len(y) > 0:
                 y = y_func(y)
             if y.ndim >= 2:
@@ -428,18 +455,19 @@ def SlidingWindow(window_len:int, stride:Union[None, int]=1, start:int=0, pad_re
                     if y.shape[d] == 1: y = np.squeeze(y, axis=d)
             if y.ndim == 3:
                 y = y.transpose(0, 2, 1)
-            return X, y
-        else: return X, None
+        if output_filter is not None:
+            X, y = output_filter(X, y)
+        return X, y
     return _inner
 
 SlidingWindowSplitter = SlidingWindow
 
 # Cell
 def SlidingWindowPanel(window_len:int, unique_id_cols:list, stride:Union[None, int]=1, start:int=0,
-                       pad_remainder:bool=False, padding_value:float=np.nan, add_padding_feature:bool=True,
-                       get_x:Union[None, int, list]=None,  get_y:Union[None, int, list]=None, y_func:Optional[callable]=None, copy:bool=False,
-                       horizon:Union[int, list]=1, seq_first:bool=True, sort_by:Optional[list]=None, ascending:bool=True,
-                       check_leakage:bool=True, return_key:bool=False, verbose:bool=True):
+                       pad_remainder:bool=False, padding:str="post", padding_value:float=np.nan, add_padding_feature:bool=True,
+                       get_x:Union[None, int, list]=None,  get_y:Union[None, int, list]=None, y_func:Optional[callable]=None,
+                       output_filter:Optional[callable]=None, copy:bool=False, horizon:Union[int, list]=1, seq_first:bool=True, sort_by:Optional[list]=None,
+                       ascending:bool=True, check_leakage:bool=True, return_key:bool=False, verbose:bool=True):
 
     """
     Applies a sliding window to a pd.DataFrame.
@@ -450,16 +478,21 @@ def SlidingWindowPanel(window_len:int, unique_id_cols:list, stride:Union[None, i
         stride              = n datapoints the window is moved ahead along the sequence. Default: 1. If None, stride=window_len (no overlap)
         start               = determines the step where the first window is applied: 0 (default), a given step (int), or random within the 1st stride (None).
         pad_remainder       = allows to pad remainder subsequences when the sliding window is applied and get_y == [] (unlabeled data).
+        padding             = 'pre' or 'post' (optional, defaults to 'pre'): pad either before or after each sequence. If pad_remainder == False, it indicates
+                              the starting point to create the sequence ('pre' from the end, and 'post' from the beginning)
         padding_value       = value (float) that will be used for padding. Default: np.nan
         add_padding_feature = add an additional feature indicating whether each timestep is padded (1) or not (0).
-        horizon             = number of future datapoints to predict:
+        horizon             = number of future datapoints to predict (y). If get_y is [] horizon will be set to 0.
                             * 0 for last step in each sub-window.
                             * n > 0 for a range of n future steps (1 to n).
                             * n < 0 for a range of n past steps (-n + 1 to 0).
                             * list : for those exact timesteps.
         get_x               = indices of columns that contain the independent variable (xs). If None, all data will be used as x.
-        get_y               = indices of columns that contain the target (ys). If None, all data will be used as y. [] means no y data is created (unlabeled data).
+        get_y               = indices of columns that contain the target (ys). If None, all data will be used as y.
+                              [] means no y data is created (unlabeled data).
         y_func              = function to calculate the ys based on the get_y col/s and each y sub-window. y_func must be a function applied to axis=1!
+        output_filter       = optional function to filter output (X (and y if available)). This is useful when some values need to be removed. The function
+                              should take X and y (even if it's None) as arguments.
         copy                = copy the original object to avoid changes in it.
         seq_first           = True if input shape (seq_len, n_vars), False if input shape (n_vars, seq_len)
         sort_by             = column/s used for sorting the array in ascending order
@@ -482,14 +515,14 @@ def SlidingWindowPanel(window_len:int, unique_id_cols:list, stride:Union[None, i
 
         if copy:
             o = o.copy()
-        o.sort_values(by=sort_by, axis=0, ascending=ascending, inplace=True, ignore_index=True)
+        o.sort_values(by=sort_by, axis=0, ascending=ascending, inplace=True, ignore_index=True, kind="mergesort")
         unique_id_values = o[unique_id_cols].drop_duplicates().values
         _x = []
         _y = []
         _key = []
         for v in progress_bar(unique_id_values, display=verbose, leave=False):
-            x_v, y_v = SlidingWindow(window_len, stride=stride, start=start, pad_remainder=pad_remainder, padding_value=padding_value,
-                                     add_padding_feature=add_padding_feature, get_x=get_x, get_y=get_y, y_func=y_func,
+            x_v, y_v = SlidingWindow(window_len, stride=stride, start=start, pad_remainder=pad_remainder, padding=padding, padding_value=padding_value,
+                                     add_padding_feature=add_padding_feature, get_x=get_x, get_y=get_y, y_func=y_func, output_filter=output_filter,
                                      horizon=horizon, seq_first=seq_first,
                                      check_leakage=check_leakage)(o[(o[unique_id_cols].values == v).sum(axis=1) == len(v)])
             if x_v is not None and len(x_v) > 0:
